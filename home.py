516293ba339d125e1f9c3b64dd93b22c8ea9dc2a
Roman Shut <shutroman23@gmail.com>
2024-02-13 18:31:52 +0300

    корректировка внешнего вида кода

--- home.py
+++ home.py
@@ -1,28 +1,30 @@
-# from selenium import webdriver
-#
-# driver = webdriver.Chrome(executable_path='C:/chromedriver.exe')
-# driver.maximize_window()
-# driver.implicitly_wait(5)
-# driver.get("https://practice.automationtesting.in/")
-#
-# driver.execute_script("window.scrollBy(0, 600);")
-#
-# SeleniumRuby = driver.find_element_by_css_selector("img[title='Selenium Ruby']")
-# SeleniumRuby.click()
-# Reviev = driver.find_element_by_css_selector(".reviews_tab a")
-# Reviev.click()
-#
-# driver.execute_script("window.scrollBy(0, 800);")
-#
-# Star_5 = driver.find_element_by_css_selector('a.star-5')
-# Star_5.click()
-# Comment = driver.find_element_by_id("comment")
-# Comment.send_keys("Nice book!")
-# Name = driver.find_element_by_id("author")
-# Name.send_keys("Ivan")
-# Email = driver.find_element_by_id("email")
-# Email.send_keys("ivan@email.com")
-# Submit = driver.find_element_by_css_selector("#submit.submit")
-# Submit.click()
-#
-# driver.quit()
+# Add comment
+
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path='C:/chromedriver.exe')
+driver.maximize_window()
+driver.implicitly_wait(5)
+driver.get("https://practice.automationtesting.in/")
+
+driver.execute_script("window.scrollBy(0, 600);")
+
+SeleniumRuby = driver.find_element_by_css_selector("img[title='Selenium Ruby']")
+SeleniumRuby.click()
+Reviev = driver.find_element_by_css_selector(".reviews_tab a")
+Reviev.click()
+
+driver.execute_script("window.scrollBy(0, 800);")
+
+Star_5 = driver.find_element_by_css_selector('a.star-5')
+Star_5.click()
+Comment = driver.find_element_by_id("comment")
+Comment.send_keys("Nice book!")
+Name = driver.find_element_by_id("author")
+Name.send_keys("Ivan")
+Email = driver.find_element_by_id("email")
+Email.send_keys("ivan@email.com")
+Submit = driver.find_element_by_css_selector("#submit.submit")
+Submit.click()
+
+driver.quit()
--- registraion_login.py
+++ registraion_login.py
@@ -1,48 +1,50 @@
 # 1 Register
 
-# from selenium import webdriver
-#
-# driver = webdriver.Chrome(executable_path='C:/chromedriver.exe')
-# driver.maximize_window()
-# driver.implicitly_wait(5)
-# driver.get("https://practice.automationtesting.in/")
-#
-# My_Account = driver.find_element_by_id("menu-item-50")
-# My_Account.click()
-#
-# Reg_Mail = driver.find_element_by_id("reg_email")
-# Reg_Mail.send_keys("ivanov@email.com")
-# Reg_Pass = driver.find_element_by_id("reg_password")
-# Reg_Pass.send_keys("7_FJucizqG")
-#
-# Register = driver.find_element_by_css_selector("[name='register']")
-# Register.click()
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path='C:/chromedriver.exe')
+driver.maximize_window()
+driver.implicitly_wait(5)
+driver.get("https://practice.automationtesting.in/")
+
+My_Account = driver.find_element_by_id("menu-item-50")
+My_Account.click()
+
+Reg_Mail = driver.find_element_by_id("reg_email")
+Reg_Mail.send_keys("ivanov@email.com")
+Reg_Pass = driver.find_element_by_id("reg_password")
+Reg_Pass.send_keys("7_FJucizqG")
+
+Register = driver.find_element_by_css_selector("[name='register']")
+Register.click()
+
+driver.quit()
 
 # 2 Login
 
-# from selenium import webdriver
-#
-# driver = webdriver.Chrome(executable_path='C:/chromedriver.exe')
-# driver.maximize_window()
-# driver.implicitly_wait(5)
-# driver.get("https://practice.automationtesting.in/")
-#
-# My_Account = driver.find_element_by_id("menu-item-50")
-# My_Account.click()
-#
-# Username = driver.find_element_by_id("username")
-# Username.send_keys("ivanov@email.com")
-# Password = driver.find_element_by_id("password")
-# Password.send_keys("7_FJucizqG")
-# Login = driver.find_element_by_name('login')
-# Login.click()
-#
-# Logout = driver.find_element_by_link_text('Logout')
-# if not Logout:
-#     print('Logout нет на на странице')
-# else:
-#     print('Logout присутствует на странице')
-#
-# driver.quit()
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path='C:/chromedriver.exe')
+driver.maximize_window()
+driver.implicitly_wait(5)
+driver.get("https://practice.automationtesting.in/")
+
+My_Account = driver.find_element_by_id("menu-item-50")
+My_Account.click()
+
+Username = driver.find_element_by_id("username")
+Username.send_keys("ivanov@email.com")
+Password = driver.find_element_by_id("password")
+Password.send_keys("7_FJucizqG")
+Login = driver.find_element_by_name('login')
+Login.click()
+
+Logout = driver.find_element_by_link_text('Logout')
+if not Logout:
+    print('Logout нет на на странице')
+else:
+    print('Logout присутствует на странице')
+
+driver.quit()
 
 
